c468849f0b0972716b5e1c42b154f5fe0c76afaf
Lucas Santos <lucas201273@gmail.com>
2024-01-21 12:55:40 -0300

    feat: add clientdto

---
+++ src/main/java/br/com/contasapagar/dtos/ClientDto.java
@@ -0,0 +1,39 @@
+package br.com.contasapagar.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClientDto implements Serializable {
+    private UUID id;
+    @NotBlank(message="The field name must be required")
+    private String name;
+    @CPF(message=" The field code must be cpf or cnpj")
+    @CNPJ(message = "The code must be cpf or cnpj")
+    private String code;
+    @Email(message = "Please provided a valid email")
+    @NotBlank(message = "The field email must be required")
+    private String email;
+    private String phone;
+    private String address;
+    @JsonProperty("sub_locality")
+    private String sublocality;
+    private String city;
+}
