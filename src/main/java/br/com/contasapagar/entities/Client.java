fdd3126bf9f86bafbad47f6f6ba6e8608641ef6c
Lucas Santos <lucas201273@gmail.com>
2024-01-21 12:52:17 -0300

    feat: add client entity and repository

---
+++ src/main/java/br/com/contasapagar/entities/Client.java
@@ -0,0 +1,33 @@
+package br.com.contasapagar.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table
+@Entity(name="tb_client")
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    @Column(unique = true)
+    private String code;
+    @Column(unique = true)
+    private String email;
+    private String phone;
+    private String address;
+    @Column(name="sub_locality")
+    private String sublocality;
+    private String city;
+}
